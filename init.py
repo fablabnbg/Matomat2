c7390c6efe0a503f091de307589d21664b2768be
Patrick Himmelmann <himmelmannp@gmail.com>
2015-05-08 23:41:49 +0200

    Fixed init.py Creation of admin user no longer fails

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ import authentication as a
 from config import dbengine
 
 s=db.create_sessionmaker(dbengine)()
-a.create_user(s,'admin','admin',None)
+s.add(db.User(name='admin',password=a.genpw('admin'),creator=None))
 s.add(db.Item(name='Mate',price='100'))
 s.add(db.Item(name='Bier',price='150'))
 s.commit()
