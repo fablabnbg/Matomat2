ae9efd58dd12f8a900bb29f4a7725a48db93e532
HimmPa <patrick.himmelmann@kit.edu>
2016-03-24 12:49:39 +0100

    Add Impressum

---
+++ client/html/impressum.html.ex
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Matomat Impressum</title>
+	</head>
+	<body>
+		<h1>Impressum</h1>
+		Beispiel für ein Impressum bitte anpassen
+	</body>
+</html>
